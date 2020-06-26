2eb045282d15b8457bab89298532e74f91d56ffc
sun132 <67191789+sun132@users.noreply.github.com>
2020-06-26 10:07:25 +0800

    smallgame2048
    smallgame2048

---
+++ shiyan/shiyan1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shiyan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] map = new int[4, 4]
+            {
+                { 0,2,2,4},
+                { 2,0,2,4},
+                { 0,4,2,0},
+                { 2,8,0,2},
+            };
+            display(map);
+            Console.ReadLine();
+        }
+        private static int[] RemoveZero(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            int t = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    newArray[t++] = array[i];
+                }
+            }
+            return newArray;
+        }
+        private static void display(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    }
+
